76780aace152207928342c37343dc843fe433a35
Jai Parera <jaiparera@gmail.com>
2022-02-26 08:42:22 -0800

    Fixed local repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Page from "./components/Post";
+
+function App() {
+  return (
+    <div className="App">
+      <Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <h1>Title</h1>
+        <p>Some body text</p>
+    </div>
+  )
+}
+
+export default Page
