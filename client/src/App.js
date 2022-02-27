200970fc970eff5fdaf1bce302602a7be7cd6799
Arush Ramteke <arushramtek@gmail.com>
2022-02-26 19:20:19 -0800

    hardcode change

--- client/src/App.js
+++ client/src/App.js
@@ -94,7 +94,7 @@ function App() {
     title: 'The Most Meaningful Text',
     subtitle: 'Truly artistic prose',
     text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-    likes: [],
+    likes: ["a", "b", "c"],
     contributor: {
       username: "MrDeez",
       id: "621aea52573732e27fbcb898"
